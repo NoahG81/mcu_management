8513633d047d584e104aaaa7c9842d13a767da83
Noah Guerreiro <noah.guerreiro1801@gmail.com>
2025-12-27 14:46:31 +0100

    first commit

---
+++ src/main/java/com/project/mcu_management/McuManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.mcu_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McuManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McuManagementApplication.class, args);
+    }
+
+}
